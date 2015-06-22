4f1fe0fda2f3df652639357df627ffdeea7cd9b8
Danny Chen <danny.chen@tadashishoji.com>
2015-06-22 13:57:08 -0700

    change truncate tables action

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -358,14 +358,14 @@ class ReturnsController < ApplicationController
 
 		tables.each do |table|
 			ActiveRecord::Base.connection.execute("DELETE FROM #{table}")
-			case self.connection.adapter_name
-      			when 'MySQL'
+			#case self.connection.adapter_name
+      		#	when 'MySQL'
         			#self.connection.execute "ALTER TABLE #{self.table_name} AUTO_INCREMENT=#{options[:to]}"
-      			when 'PostgreSQL'
-        			ActiveRecord::Base.connection.execute "ALTER SEQUENCE #{table}_id_seq RESTART WITH 1;"
-      			when 'SQLite'
-        			ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence where name='#{table}'")
-      		else
+      		#	when 'PostgreSQL'
+        	#		ActiveRecord::Base.connection.execute "ALTER SEQUENCE #{table}_id_seq RESTART WITH 1;"
+      		#	when 'SQLite'
+        	#		ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence where name='#{table}'")
+      		#else
     
 			
 		end
