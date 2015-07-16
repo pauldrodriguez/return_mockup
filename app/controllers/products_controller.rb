0bda5f3e902f6e6dbb9371b072af539773b63a34
Danny Chen <danny.chen@tadashishoji.com>
2015-07-16 11:43:09 -0700

    fixes for backend product management

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
           ids_to_delete =params[:delete_area_ids].split("_")
           BoxArea.destroy(ids_to_delete)
         end
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to edit_product_path(@product) }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
