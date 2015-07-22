fe08fd28a9c5a4e6ee237e39fdbcab466d4d3fcf
Danny Chen <danny.chen@tadashishoji.com>
2015-07-22 11:30:55 -0700

    more quantity for product 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Order.create(order_num: 12345, amount: 800.99, first_name: "paul",last_name: "rodriguez",discounts: 0.00,tax: 0.00,subtotal: 800.99)
-OrderItem.create(order_id: 1,product_id: 1, order_num: 12345, product_name: "Product 1", size: "14Q", quantity: 1,original_price: 800.99,price: 800.99,status: "shipped")
+OrderItem.create(order_id: 1,product_id: 1, order_num: 12345, product_name: "Product 1", size: "14Q", quantity: 3,original_price: 800.99,price: 800.99,status: "shipped")
 OrderItem.create(order_id: 1,product_id: 2,order_num: 12345, product_name: "Product 2", size: "10A", quantity: 2,original_price: 700.99,price: 700.99,status:"shipped")
 OrderItem.create(order_id: 1,product_id: 3,order_num: 12345, product_name: "Product 3", size: "10A", quantity: 2,original_price: 700.99,price: 700.99,status:"shipped")
 
